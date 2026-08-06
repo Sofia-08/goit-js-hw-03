4145450f5c26ada66a9ae55e229a75000206c1f6
Lemishchak Sofia <165265574+Sofia-08@users.noreply.github.com>
2026-08-06 14:50:34 +0300

    Eigth homework

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const list = document.querySelectorAll('.item');
-console.log('Number of categories: ', list.length);
-list.forEach(item => {
-  const categoryTitle = item.querySelector('h2').textContent;
-  const elementsCount = item.querySelectorAll('ul li').length;
-  console.log(`Category: ${categoryTitle}`);
-  console.log(`Elements: ${elementsCount}`);
-});
